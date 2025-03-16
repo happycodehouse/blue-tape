d68448fbcf3c3033b6ba75a7e469f4c94a008632
hadam jang <happycodehouse@gmail.com>
2025-03-17 01:08:31 +0900

    WIP HEADER

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,7 +3,17 @@ import headerStyle from "../../styles/layout/_header.module.scss";
 const Header = () => {
     return (
         <header className={headerStyle.header}>
-            Header Content
+            <div className={headerStyle.header_inner}>
+                <div className={headerStyle.logo}>BLUE TAPE</div>
+                <ul className={headerStyle.gnb}>
+                    <li>
+                        <a href="">ABOUT</a>
+                    </li>
+                    <li>
+                        <a href="">WORKS</a>
+                    </li>
+                </ul>
+            </div>
         </header>
     );
 }
