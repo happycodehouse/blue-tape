98e5ff9a1ee418a08739d3d38dbd5a25f0fa8db4
happycodehouse <happycodehouse@gmail.com>
2025-09-11 16:09:08 +0900

    svg > png

--- src/components/home/BearAnimation.jsx
+++ src/components/home/BearAnimation.jsx
@@ -1,17 +1,17 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {gsap} from 'gsap';
 import style from "./bearAnimation.module.scss";
-import figure from "/src/assets/images/main/bear-motorcycle.svg";
-import mass01 from "/src/assets/images/main/mass-01.svg";
-import mass02 from "/src/assets/images/main/mass-02.svg";
-import mass03 from "/src/assets/images/main/mass-03.svg";
-import mass04 from "/src/assets/images/main/mass-04.svg";
-import mass05 from "/src/assets/images/main/mass-05.svg";
-import mass06 from "/src/assets/images/main/mass-06.svg";
-import mass07 from "/src/assets/images/main/mass-07.svg";
-import mass08 from "/src/assets/images/main/mass-08.svg";
-import mass09 from "/src/assets/images/main/mass-09.svg";
-import mass10 from "/src/assets/images/main/mass-10.svg";
+import figure from "/src/assets/images/main/bear-motorcycle.png";
+import mass01 from "/src/assets/images/main/mass-01.png";
+import mass02 from "/src/assets/images/main/mass-02.png";
+import mass03 from "/src/assets/images/main/mass-03.png";
+import mass04 from "/src/assets/images/main/mass-04.png";
+import mass05 from "/src/assets/images/main/mass-05.png";
+import mass06 from "/src/assets/images/main/mass-06.png";
+import mass07 from "/src/assets/images/main/mass-07.png";
+import mass08 from "/src/assets/images/main/mass-08.png";
+import mass09 from "/src/assets/images/main/mass-09.png";
+import mass10 from "/src/assets/images/main/mass-10.png";
 
 const BearAnimation = () => {
   const images = [mass01, mass02, mass03, mass04, mass05, mass06, mass07, mass08, mass09, mass10];
